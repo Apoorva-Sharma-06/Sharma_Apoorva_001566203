5c2c3cd08b87c2158cddfac331b51ca9070f9fdc
Apoorva Sharma <sharma.apoo@northeastern.edu>
2021-04-21 00:19:32 +0530

    Added roles to Customer

--- FinalProject/src/Users/CustomerDirectory.java
+++ FinalProject/src/Users/CustomerDirectory.java
@@ -18,8 +18,17 @@ public class CustomerDirectory {
         custList = new ArrayList<>();
     }
     
-    public void addCustomer(String name, String username, String password, String pno, String address, String current_address, String email){
-        custList.add(new Customer(name, username, password, pno, address, current_address, email));
+    public void addCustomer(String name, String username, String password, String pno, String address, String current_address, String email, String role){
+        custList.add(new Customer(name, username, password, pno, address, current_address, email, role));
+    }
+    
+    public String getRoleFromLogin(String username, String password){
+        for(int i=0; i<custList.size(); i++){
+            if(custList.get(i).username.equals(username) && custList.get(i).password.equals(password)){
+                return custList.get(i).role;
+            }
+        }
+        return "UserNotFound";
     }
     
     

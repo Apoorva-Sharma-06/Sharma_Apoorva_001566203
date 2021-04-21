ec5cd42dbfd740a6fded9dc497594a48e70307be
Apoorva Sharma <sharma.apoo@northeastern.edu>
2021-04-21 19:50:17 +0530

    Added getters and setters for new customer attributes

--- FinalProject/src/Users/Customer.java
+++ FinalProject/src/Users/Customer.java
@@ -34,6 +34,88 @@ public class Customer {
         count++;
     }
 
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Customer.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPno() {
+        return pno;
+    }
+
+    public void setPno(String pno) {
+        this.pno = pno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCurrent_address() {
+        return current_address;
+    }
+
+    public void setCurrent_address(String current_address) {
+        this.current_address = current_address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+
     
     
 }

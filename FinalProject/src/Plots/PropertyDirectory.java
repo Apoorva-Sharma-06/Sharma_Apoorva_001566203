f42c3e123d077c1d1fa4e3af912ede2eaa1ba445
Apoorva Sharma <sharma.apoo@northeastern.edu>
2021-04-23 20:07:36 +0530

    Added Property and Property Directory

---
+++ FinalProject/src/Plots/PropertyDirectory.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Plots;
+
+import Dept_LegalVerification.LegalVerification;
+import Dept_LegalVerification.LegalVerificationDirectory;
+import Dept_PhysicalVerification.PhysicalVerificationDirectory;
+import Dept_Presenters.PresentProperty;
+import Dept_Presenters.PresentPropertyDirectory;
+import Users.User;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Apoorva Sharma <your.name at your.org>
+ */
+public class PropertyDirectory {
+    private ArrayList<Property> propertyList;
+    PhysicalVerificationDirectory pvd;
+    PresentPropertyDirectory pp;
+    LegalVerificationDirectory lv;
+    
+    public PropertyDirectory(){
+        propertyList = new ArrayList<>();
+        pvd = new PhysicalVerificationDirectory(this);
+        pp = new PresentPropertyDirectory();
+        lv = new LegalVerificationDirectory(this);
+    }
+    
+    public void addProperty(int ptype, String area, String neighbourhood, String city, String state, String zip, double size, double cost, int btype, int advOpt, User seller, String sellerDocs){
+        Property p = new Property(ptype, area, neighbourhood, city, state, zip, size, cost, btype, advOpt, seller, sellerDocs);
+        propertyList.add(p);
+        pvd.addPropertyForVerification(p, seller);
+    }
+    
+    public ArrayList<Property> getPropertyList(int ptype, String area, String neighbourhood, String city, String state, int btype){
+        ArrayList<Property> templist = new ArrayList<>();
+        for(int i=0; i<templist.size(); i++){
+            if(propertyList.get(i).getPtype() == ptype &&
+                    propertyList.get(i).getBtype() == btype &&
+                    propertyList.get(i).getIsPhysicalVerificationPending() == 0 &&
+                    propertyList.get(i).getBuyerInterested() == 0 &&
+                    propertyList.get(i).getArea().equals(area) &&
+                    propertyList.get(i).getCity().equals(city) &&
+                    propertyList.get(i).getState().equals(state) &&
+                    propertyList.get(i).getNeighbourhood().equals(neighbourhood)
+                    ){
+                templist.add(propertyList.get(i));
+            }
+        }
+        return templist;
+    }
+    
+    public void showInterest(Property p, User buyer, String buyerdocs){
+        p.setBuyer(buyer);
+        p.setBuyerDocs(buyerdocs);
+        p.setBuyerInterested(1);    
+    }
+    
+    public void optForPresenter(Property p, User buyer){
+        pp.addPropertyForPresentation(p, buyer);
+    }
+    
+    public ArrayList<Property> getPropertyListForDigitalAdvertising(){
+        ArrayList<Property> templist = new ArrayList<>();
+        for(int i=0; i<templist.size(); i++){
+            if(propertyList.get(i).getAdvOpt() == 1){
+                templist.add(propertyList.get(i));
+            }
+        }
+        return templist;
+    }
+    
+    public ArrayList<Property> getPropertyList(){
+        return this.propertyList;
+    }
+}

28227d2194b2535cddf3b84e9c848bb164892531
Apoorva Sharma <sharma.apoo@northeastern.edu>
2021-04-24 00:25:28 +0530

    Updated user directory

---
+++ FinalProject/src/Users/UserDirectory.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Users;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Apoorva Sharma <your.name at your.org>
+ */
+public class UserDirectory {
+    private ArrayList<User> userList;
+
+    public UserDirectory() {
+        userList = new ArrayList<>();
+    }
+    
+    public void addUser(String name, String username, String password, String pno, String address, String current_address, String email, String role){
+        userList.add(new User(name, username, password, pno, address, current_address, email, role));
+    }
+    
+    public String getRoleFromLogin(String username, String password){
+        for(int i=0; i<userList.size(); i++){
+            if(userList.get(i).username.equals(username) && userList.get(i).password.equals(password)){
+                return userList.get(i).role;
+            }
+        }
+        return "UserNotFound";
+    }
+
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<User> userList) {
+        this.userList = userList;
+    }
+    
+    public void removeCust(Customer c){
+        userList.remove(c);
+    }
+    
+}

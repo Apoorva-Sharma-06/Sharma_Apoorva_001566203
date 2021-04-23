d5d72f303d91b169321438af70c51ea61d5a01f1
Apoorva Sharma <sharma.apoo@northeastern.edu>
2021-04-24 00:24:49 +0530

    Added User.java

--- FinalProject/src/Users/User.java
+++ FinalProject/src/Users/User.java
@@ -10,5 +10,106 @@ package Users;
  * @author Apoorva Sharma <your.name at your.org>
  */
 public class User {
-    
+    private static int count = 1;
+    String name;
+    String username;
+    String password;
+    String pno;
+    String address;
+    String current_address;
+    String email;
+    int id;
+    String role;
+
+    public User(String name, String username, String password, String pno, String address, String current_address, String email, String role) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.pno = pno;
+        this.address = address;
+        this.current_address = current_address;
+        this.email = email;
+        this.id = count;
+        this.role = role;
+        count++;
+    }
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        User.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPno() {
+        return pno;
+    }
+
+    public void setPno(String pno) {
+        this.pno = pno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCurrent_address() {
+        return current_address;
+    }
+
+    public void setCurrent_address(String current_address) {
+        this.current_address = current_address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }  
 }

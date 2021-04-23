e2c5c8b2bdd04982ea1708ce22096f7faa244fea
Apoorva Sharma <sharma.apoo@northeastern.edu>
2021-04-23 20:06:27 +0530

    Added Legal Department

---
+++ FinalProject/src/Dept_LegalVerification/LegalVerification.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dept_LegalVerification;
+
+import Plots.Property;
+
+/**
+ *
+ * @author Apoorva Sharma <your.name at your.org>
+ */
+public class LegalVerification {
+    Property p;
+
+    public LegalVerification(Property p) {
+        this.p = p;
+    }
+    
+    
+}
---
+++ FinalProject/src/Dept_LegalVerification/LegalVerificationDirectory.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dept_LegalVerification;
+
+
+import Dept_CAVerification.CAVerificationDirectory;
+import Plots.Property;
+import Plots.PropertyDirectory;
+import Users.User;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Apoorva Sharma <your.name at your.org>
+ */
+public class LegalVerificationDirectory {
+    ArrayList<LegalVerification> verificationList;
+    CAVerificationDirectory cd;
+    PropertyDirectory pd;
+
+    public LegalVerificationDirectory(PropertyDirectory pd) {
+        this.verificationList = new ArrayList<>();
+        cd = new CAVerificationDirectory(pd);
+        this.pd = pd;
+    }
+    
+    public void addPropertyForVerification(Property p){
+        verificationList.add(new LegalVerification(p));
+    }
+
+    public ArrayList<LegalVerification> getVerificationList() {
+        return verificationList;
+    }
+    
+    public void verifyProperty(LegalVerification p, int result){
+        if(result == 0){
+            //Send email to buyer that docs not verified
+            p.p.setBuyer(null);
+            p.p.setBuyerDocs(null);
+            p.p.setBuyerInterested(0);
+        }
+        else {
+            p.p.setInCAVerificationPending(1);
+            cd.addPropertyForVerification(p.p);
+        }
+        verificationList.remove(p);
+    }
+    
+    
+}
